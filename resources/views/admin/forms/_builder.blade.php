fa8f23dce3daf7f351e4e8a7f2cb81901f4d3252
apikapikk <mmuwaffiqulihsan@gmail.com>
2025-10-01 09:31:36 +0700

    add more desc

--- resources/views/admin/forms/_builder.blade.php
+++ resources/views/admin/forms/_builder.blade.php
@@ -20,7 +20,7 @@ $initialFields = old('fields')
             <x-text-input name="name" value="{{ old('name', $form->name ?? '') }}" class="mt-1 w-full" required />
         </div>
         <div>
-            <x-input-label value="Slug (samakan dengan label namun jangan gunakan spasi)" />
+            <x-input-label value="Slug (samakan dengan label namun jangan gunakan spasi atau gunakan _ untuk spasinya)" />
             <x-text-input name="slug" value="{{ old('slug', $form->slug ?? '') }}" class="mt-1 w-full" />
         </div>
         <div class="sm:col-span-2">
